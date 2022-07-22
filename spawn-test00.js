968699f8e8f103ab94e06714447c7cb2f2aeeac9
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-22 13:30:51 -0400

    rev

---
+++ spawn-test00.js
@@ -0,0 +1,25 @@
+const config = require('config')
+const {spawn} = require('child_process')
+
+const srcDir = config.get('srcDir')
+const gdalmergePath = config.get('gdalmergePath')
+
+
+
+
+let gdalmergeArray = [
+    '-o', 'test-merged.tif' 
+]
+var inputSrc = new Array(
+    `${srcDir}/s04_e010_1arc_v3.tif`,
+    `${srcDir}/s04_e011_1arc_v3.tif`
+)
+gdalmergeArray = gdalmergeArray.concat(inputSrc)
+
+const gdalmerge = spawn(gdalmergePath,gdalmergeArray)
+gdalmerge.on('exit', () => {
+    console.log('end')
+    console.log(gdalmergeArray)
+})
+
+//gdalmerge
