318ab35dc630ae666d6a6a8eedc9d846470bbfa6
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-22 18:08:42 -0400

    rev

--- prep-list01.js
+++ prep-list01.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const config = require('config')
 
 const srcDir = config.get('srcDir')
+//const srtmFiles = config.get('srtmFiles')
 
 var fileList = fs.readdirSync(srcDir)
 fileList = fileList.filter(r => r.indexOf('.tif') !== -1)
---
+++ prep-list02.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const config = require('config')
+
+const srcDir = config.get('srcDir')
+const srtmFiles = config.get('srtmFiles')
+
+var fileList = fs.readdirSync(srcDir)
+fileList = fileList.filter(r => r.indexOf('.tif') !== -1)
+
+let srtmFiles2 = []
+for (let i=0; i<srtmFiles.length; i++){
+    //srtmfile = srtmfile.replace('SRTM','ww')
+    //srtmFiles[i] = srtmFiles[i].replace('SRTM','ww')
+    //console.log(srtmFiles[i].replace('SRTM1','').replace('W','_w').replace('E','_e').replace('V3','_1arc_v3.tif').toLowerCase())
+    srtmFiles2.push(srtmFiles[i].replace('SRTM1','').replace('W','_w').replace('E','_e').replace('V3','_1arc_v3.tif').toLowerCase())
+}
+
+console.log(srtmFiles)
+console.log(srtmFiles2)
+console.log(srtmFiles2.length)
+console.log(srtmFiles.length)
+
+console.log(fileList)
+console.log(fileList.length)
