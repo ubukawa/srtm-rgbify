7935500bafb4cf00db9728f0e20c5ba38af506f9
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-25 11:24:39 -0400

    rev

--- queue-01.js
+++ queue-01.js
@@ -2,6 +2,8 @@
 const config = require('config')
 const fs = require('fs')
 const tilebelt = require('@mapbox/tilebelt')
+const Queue = require('better-queue')
+const { resolve } = require('path')
 
 //config parameters
 const srcDir = config.get('srcDir')
@@ -9,6 +11,12 @@ const srcDir = config.get('srcDir')
 let modulesObj = {} //object {key: [srcFile, ... ], ...}
 let emptyModules = []
 let keys = [] //Array of key such as "6-x-y"
+let keyInProgress = []
+let idle = true
+
+const isIdle = () => {
+    return idle
+}
 
 let fileList = fs.readdirSync(srcDir) //list from the src folder
 fileList = fileList.filter(r => r.indexOf('.tif') !== -1) //only tiff file
@@ -92,3 +100,64 @@ for (const key of keys){
 
 console.log(`-------UNVT---------------\nProduction starts: \n- From the saved sources, we have ${Object.keys(modulesObj).length} modules with SRTM DEM. \n- ${emptyModules.length} modules are without SRTM DEM.\n--------------------------`)
 console.log(`Here is the list of ${Object.keys(modulesObj).length} modules: \n${Object.keys(modulesObj)}`) 
+
+
+
+
+const queue = new Queue(async (t, cb) => {
+    const startTime = new Date()
+    const key = t.key
+    const tile = t.tile
+    const [z, x, y] = tile
+
+    keyInProgress.push(key)
+    console.log(`[${keyInProgress}] in progress`)
+
+    console.log(`${key}: ${modulesObj[key].length}\n(${modulesObj[key]})`) //list of src files
+
+
+    const sample = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve()
+            }, 500)
+    })
+
+    sample.then(() => {
+        keyInProgress = keyInProgress.filter((v) => !(v === key))  
+        return cb()
+    })
+
+    },{
+    concurrent: 3,
+    maxRetries: 3,
+    retryDelay: 5000
+    })
+
+const queueTasks = () => {
+    for (let module of Object.keys(modulesObj)){
+    //for (let tile of [[6,32,20],[6,32,21],[6,32,22],[6,32,23],[6,33,20],[6,33,21],[6,33,22]]){
+    //for (let key of ['bndl1', 'bndl2', 'bndl3', 'bndl4', 'bndl5', 'bndl6']){
+        let tile = module.split('-').map(v => Number(v))
+        queue.push({
+            key: module,
+            tile: tile
+        })
+    }
+}
+
+const shutdown = () => {
+    console.log('** production system shutdown! (^_^) **')
+  }
+
+  const main = async () =>{
+    const stTime = new Date()
+    console.log(`${stTime.toISOString()}: Production starts. `)
+    queueTasks()
+    queue.on('drain', () => {
+        const closeTime = new Date()
+        console.log(`Production ends: ${stTime.toISOString()} --> ${closeTime.toISOString()}`)
+        shutdown()
+    })
+}
+
+main()
